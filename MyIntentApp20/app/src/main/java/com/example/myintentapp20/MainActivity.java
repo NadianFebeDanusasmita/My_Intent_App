7f86daa1e39e635a8e7301e45b2941e3ea517bae
NadianFebeDanusasmita <60424678+NadianFebeDanusasmita@users.noreply.github.com>
2020-01-29 03:15:13 -0600

    My_Intent_App

---
+++ MyIntentApp20/app/src/main/java/com/example/myintentapp20/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myintentapp20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+public class MainActivity extends AppCompatActivity implements_View {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
